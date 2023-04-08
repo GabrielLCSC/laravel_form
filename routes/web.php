79d32769db46f0159d97f3c9d780e3d5e2a08e1b
Gabriel Dautreppe <gabriel.dautreppe@gmail.com>
2023-04-08 10:39:39 +0200

    :sparkles: add routes + methods

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/contact', [ContactController::class, 'create']);
+Route::post('/contact', [ContactController::class, 'store']);
+
+Route::post('/contact-confirm', function () {
+    return view('contact-confirm');
+});
