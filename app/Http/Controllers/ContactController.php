24923b1f503ace80e9f6d767dce9faf6607114b7
Gabriel Dautreppe <gabriel.dautreppe@gmail.com>
2023-04-10 12:03:49 +0200

    💥 (add function store +begin bdd set up): add function store +begin bdd set up
    creating the method in charge of storing and sending the data to the database

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -10,4 +10,31 @@ class ContactController extends Controller
     {
         return view('contact');
     }
+    public function store(Request $request)
+    {
+        // Valider les données du formulaire de contact
+        $data = $request->validate([
+            'name' => 'required',
+            'email' => 'required|email',
+            'message' => 'required',
+        ], [
+            'name.required' => 'Le nom est obligatoire',
+            'email.required' => 'L\'email est obligatoire',
+            'email.email' => 'L\'email n\'est pas valide',
+            'message.required' => 'Le message est obligatoire',
+        ]);
+
+        // Créer un nouvel objet Contact pour stocker les données du formulaire
+        $contact = new Contact();
+        $contact->name = $request->input('name');
+        $contact->email = $request->input('email');
+        $contact->message = $request->input('message');
+        $contact->save();
+
+        // dd($contact);
+
+
+        // Rediriger l'utilisateur vers la page de confirmation
+        return view('contact-confirm', ['contact' => $contact]);
+    }
 }
